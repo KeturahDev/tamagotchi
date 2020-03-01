bb4ca68e4fb4f95cc142527fe5eda5f20329fd5b
Keturah Howard <keturah.dev@gmail.com>
2020-02-29 19:50:11 -0800

    refactor frontend

--- src/main.js
+++ src/main.js
@@ -4,70 +4,66 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
+function displayToGameScreen(student) {
+  $('#studentName').html(student.name);
+  $('#level').html(student.level);
+}
+function meterBar(student) {
+  let drain = setInterval(() => {
+    $('#coffeeBar').css("width", student.energyLevel + "0%");
+    $('#studyBar').css("width", student.studyLevel + "0%");
+    $('#networkBar').css("width", student.networkLevel + "0%");
+    $('#level').html(student.level);
+
+
+    if (student.networkLevel <= 0 || student.studyLevel <= 0 || student.energyLevel <= 0) {
+      $('#happy').hide();
+      $('#lonely').hide();
+      $('#dumb').hide();
+      $('#tired').hide();
+      $('#grad').hide();
+      $('#dead').show();
+    } else if (student.energyLevel <= student.max / 2) {
+      $('#happy').hide();
+      $('#lonely').hide();
+      $('#dumb').hide();
+      $('#tired').show();
+      $('#grad').hide();
+      $('#dead').hide();
+    } else if (student.studyLevel <= student.max / 2) {
+      $('#happy').hide();
+      $('#lonely').hide();
+      $('#dumb').show();
+      $('#tired').hide();
+      $('#grad').hide();
+      $('#dead').hide();
+    } else if (student.networkLevel <= student.max / 2) {
+      $('#happy').hide();
+      $('#lonely').show();
+      $('#dumb').hide();
+      $('#tired').hide();
+      $('#grad').hide();
+      $('#dead').hide();
+    } else if (student.level >= 7) {
+      $('#happy').hide();
+      $('#lonely').hide();
+      $('#dumb').hide();
+      $('#tired').hide();
+      $('#grad').show();
+      $('#dead').hide();
+      clearInterval(drain);
+    } else if (student.energyLevel > student.max / 2 && student.studyLevel > student.max / 2 && student.studyLevel > student.max / 2) {
+      $('#happy').show();
+      $('#lonely').hide();
+      $('#dumb').hide();
+      $('#tired').hide();
+      $('#grad').hide();
+      $('#dead').hide();
+    }
+  }, 1000);
+}
 
 $(document).ready(function () {
-  let level = $('#level');
-  function displayToGameScreen(student) {
-    $('#studentName').html(student.name);
-    level.html(student.level);
-  }
-  function meterBar(student) {
-    let drain = setInterval(() => {
-    // setInterval(() => {
-      // $("#coffeeBar").text(student.energyLevel + "0%");
-      console.log(student.energyLevel + "0%");
-      // $("#studyBar").text(student.studyLevel + "0%");
-      // $("#networkBar").text(student.networkLevel + "0%");
-      $('#coffeeBar').css("width", student.energyLevel + "0%");
-      $('#studyBar').css("width", student.studyLevel + "0%");
-      $('#networkBar').css("width", student.networkLevel + "0%");
-      level.html(student.level);
-      if (student.networkLevel <= 0 || student.studyLevel <= 0 || student.energyLevel <= 0) {
-        $('#happy').hide();
-        $('#lonely').hide();
-        $('#dumb').hide();
-        $('#tired').hide();
-        $('#grad').hide();
-        $('#dead').show();
-      } else if (student.energyLevel <= student.max / 2) {
-        $('#happy').hide();
-        $('#lonely').hide();
-        $('#dumb').hide();
-        $('#tired').show();
-        $('#grad').hide();
-        $('#dead').hide();
-      } else if (student.studyLevel <= student.max / 2) {
-        $('#happy').hide();
-        $('#lonely').hide();
-        $('#dumb').show();
-        $('#tired').hide();
-        $('#grad').hide();
-        $('#dead').hide();
-      } else if (student.networkLevel <= student.max / 2) {
-        $('#happy').hide();
-        $('#lonely').show();
-        $('#dumb').hide();
-        $('#tired').hide();
-        $('#grad').hide();
-        $('#dead').hide();
-      } else if (student.level >= 7) {
-        $('#happy').hide();
-        $('#lonely').hide();
-        $('#dumb').hide();
-        $('#tired').hide();
-        $('#grad').show();
-        $('#dead').hide(); 
-        clearInterval(drain);
-      } else if (student.energyLevel > student.max / 2 && student.studyLevel > student.max / 2 && student.studyLevel > student.max / 2) {
-        $('#happy').show();
-        $('#lonely').hide();
-        $('#dumb').hide();
-        $('#tired').hide();
-        $('#grad').hide();
-        $('#dead').hide(); 
-      }  
-    }, 1000);
-  }
   let student;
   $('#start').submit(function (event) {
     event.preventDefault();
@@ -80,6 +76,7 @@ $(document).ready(function () {
     displayToGameScreen(student);
     $('.gamescreen').show();
     $('.startscreen').hide();
+    $('.jumbotron').hide();
 
     console.log(student);
     $('button#coffee').click(function () {
